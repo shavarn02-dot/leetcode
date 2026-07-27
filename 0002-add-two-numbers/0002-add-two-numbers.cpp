19ddeeecd5ee03b1529e77c631337c2f6bf6aa83
Sarthak <shavarn02@gmail.com>
2026-07-27 23:52:11 +0530

    Time: 2 ms (39.18%), Space: 77.1 MB (46.32%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,29 +14,21 @@ public:
 
         ListNode* temp = new ListNode(0);
         ListNode* tail = temp;
-
         int carry = 0;
-
         while (l1 || l2 || carry) {
-
             int sum = carry;
-
             if (l1) {
                 sum += l1->val;
-                l1 = l1->next;
+                 l1 = l1->next;
             }
-
             if (l2) {
                 sum += l2->val;
                 l2 = l2->next;
             }
-
             carry = sum / 10;
-
-            tail->next = new ListNode(sum % 10);
+              tail->next = new ListNode(sum % 10);
             tail = tail->next;
         }
-
-        return temp->next;
+ return temp->next;
     }
 };
