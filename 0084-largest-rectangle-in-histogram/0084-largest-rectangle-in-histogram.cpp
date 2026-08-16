36cd395719f89126084dc649998b6104569501b5
Sarthak <shavarn02@gmail.com>
2026-08-17 00:04:18 +0530

    Time: 13 ms (89.54%), Space: 78.6 MB (99.52%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -5,18 +5,12 @@ public:
         stack<int> st;
         int maxi=0;
         for(int i=n-1;i>=0;i--){
-            //int largerect=0;
+            
           
             while(!st.empty() && heights[st.top()]>=heights[i]){
-            
-                
-                //largerect+=1;
                 int index=st.top();
                 int height=heights[index];
-
                 st.pop();
-                //int rightboundary=st.top();
-                
                 int leftboundary=i;
                 int rightboundary;
                 if(st.empty())
@@ -35,9 +29,7 @@ public:
             while(!st.empty()) {
             int index=st.top();
                 int height=heights[index];
-
                 st.pop();
-                
                 int leftboundary=-1;
                 int rightboundary;
                 if(st.empty())
